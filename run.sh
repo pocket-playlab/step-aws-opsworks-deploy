3f933dec7ec0f243d5fac23d2f593710ed65a75c
Charles Martinot <charles@pocketplaylab.com>
2014-05-15 16:02:02 +0700

    Added control of variables Added import in wrecker.yml file

--- run.sh
+++ run.sh
@@ -2,9 +2,52 @@
 
 set -e
 
-export AWS_DEFAULT_REGION='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION'
-export AWS_ACCESS_KEY_ID='$WERCKER_AWS_OPSWORKS_DEPLOY_ACCESS_KEY_ID'
-export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_SECRET_ACCESS_KEY'
-export AWS_DEFAULT_OUTPUT='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT'
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_ACCESS_KEY_ID" ]
+then
+	error 'Please specify access key id'
+	exit 1
+else
+	export AWS_ACCESS_KEY_ID='$WERCKER_AWS_OPSWORKS_DEPLOY_ACCESS_KEY_ID'
+fi
 
-aws opsworks create-deployment --stack-id '$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID' --app-id '$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID' --instance-ids '$WERCKER_AWS_OPSWORKS_DEPLOY_INSTANCE_ID' --command "{\"Name\":\"deploy\"}"
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_SECRET_ACCESS_KEY" ]
+then
+	error 'Please specify secret access key'
+	exit 1
+else
+	export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_SECRET_ACCESS_KEY'
+fi
+
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID" ]
+then
+	error 'Please specify stack id'
+	exit 1
+else
+	export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID'
+fi
+
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID" ]
+then
+	error 'Please specify app id'
+	exit 1
+else
+	export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID'
+fi
+
+if [ -n "$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION" ]
+then
+	export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION'
+fi
+
+if [ -n "$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT" ]
+then
+	export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT'
+fi
+
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_INSTANCE_ID" ]
+then
+	aws opsworks create-deployment --stack-id '$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID' --app-id '$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID' --command "{\"Name\":\"deploy\"}"
+else
+	aws opsworks create-deployment --stack-id '$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID' --app-id '$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID' --instance-ids '$WERCKER_AWS_OPSWORKS_DEPLOY_INSTANCE_ID' --command "{\"Name\":\"deploy\"}"
+fi
+
