b274d56d25d1691da29294edc79322cf6847cdb3
Charles Martinot <charles@pocketplaylab.com>
2014-05-16 14:07:59 +0700

    Added logging

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-export AWS_DEFAULT_REGION='us-east-1'
-export AWS_DEFAULT_OUTPUT='json'
-
 if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_ACCESS_KEY_ID" ]
 then
 	error 'Please specify access key id'
@@ -37,24 +34,26 @@ else
 	export AWS_SECRET_ACCESS_KEY='$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID'
 fi
 
-if [ -n "$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION" ]
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION" ]
 then
-	export AWS_DEFAULT_REGION='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION'
-else
 	export AWS_DEFAULT_REGION='us-east-1'
+else
+	export AWS_DEFAULT_REGION='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_REGION'
 fi
 
-if [ -n "$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT" ]
+if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT" ]
 then
-	export AWS_DEFAULT_OUTPUT='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT'
-else
 	export AWS_DEFAULT_OUTPUT='json'
+else
+	export AWS_DEFAULT_OUTPUT='$WERCKER_AWS_OPSWORKS_DEPLOY_DEFAULT_OUTPUT'
 fi
 
 if [ ! -n "$WERCKER_AWS_OPSWORKS_DEPLOY_INSTANCE_ID" ]
 then
+	echo "Deploying on all instances"
 	aws opsworks create-deployment --stack-id '$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID' --app-id '$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID' --command "{\"Name\":\"deploy\"}"
 else
+	echo "Deploying on specified instances"
 	aws opsworks create-deployment --stack-id '$WERCKER_AWS_OPSWORKS_DEPLOY_STACK_ID' --app-id '$WERCKER_AWS_OPSWORKS_DEPLOY_APP_ID' --instance-ids '$WERCKER_AWS_OPSWORKS_DEPLOY_INSTANCE_ID' --command "{\"Name\":\"deploy\"}"
 fi
 
